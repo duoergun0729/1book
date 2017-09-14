94f07f395f82f45695306848c1d43dff286354f5
maidou <250261953@qq.com>
2017-09-14 10:25:49 +0800

    add 11-2.py

--- code/11-2.py
+++ code/11-2.py
@@ -1,16 +1,26 @@
-import pyfpgrowth
+from apriori import apriori
+from apriori import generateRules
+import re
 
-transactions = [[1, 2, 5],
-                [2, 4],
-                [2, 3],
-                [1, 2, 4],
-                [1, 3],
-                [2, 3],
-                [1, 3],
-                [1, 2, 3, 5],
-                [1, 2, 3]]
+if __name__ == '__main__':
+    #myDat = [ [ 1, 3, 4 ], [ 2, 3, 5 ], [ 1, 2, 3, 5 ], [ 2, 5 ] ]
+    myDat=[]
+    #L, suppData = apriori(myDat, 0.5)
+    #rules = generateRules(L, suppData, minConf=0.7)
+    #print 'rules:\n', rules
+    with open("../data/xss-2000.txt") as f:
+        for line in f:
+            #/discuz?q1=0&q3=0&q2=0%3Ciframe%20src=http://xxooxxoo.js%3E
+            index=line.find("?")
+            if index>0:
+                line=line[index+1:len(line)]
+                #print line
+                tokens=re.split('\=|&|\?|\%3e|\%3c|\%3E|\%3C|\%20|\%22|<|>|\\n|\(|\)|\'|\"|;|:|,|\%28|\%29',line)
+                #print "token:"
+                #print tokens
+                myDat.append(tokens)
+        f.close()
 
-patterns = pyfpgrowth.find_frequent_patterns(transactions, 2)
-rules = pyfpgrowth.generate_association_rules(patterns, 0.7)
-
-print rules
+    L, suppData = apriori(myDat, 0.15)
+    rules = generateRules(L, suppData, minConf=0.6)
+    #print 'rules:\n', rules
