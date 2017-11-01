d1661e4950ec9f592ffbf55f2759390c719a0f8f
duoergun0729 <250261953@qq.com>
2017-11-01 21:41:04 +0800

    文件顺序调整

--- code/15-3.py
+++ code/15-3.py
@@ -1,84 +1,34 @@
 import tensorflow as tf
 import pickle
 import gzip
-
-
-
-def get_one_hot(x,size=10):
-    v=[]
-    for x1 in x:
-        x2=[0]*size
-        x2[(x1-1)]=1
-        v.append(x2)
-    return v
-
+from tensorflow.contrib.learn.python import learn
+from sklearn import metrics
 
 def load_data():
     with gzip.open('../data/MNIST/mnist.pkl.gz') as fp:
         training_data, valid_data, test_data = pickle.load(fp)
     return training_data, valid_data, test_data
 
-training_data, valid_data, test_dat=load_data()
 
-x_training_data,y_training_data=training_data
-x1,y1=test_dat
+def main(unused_argv):
+    training_data, valid_data, test_dat=load_data()
 
-y_training_data=get_one_hot(y_training_data)
-y1=get_one_hot(y1)
+    x_training_data,y_training_data=training_data
+    x1,y1=test_dat
 
+    feature_columns = learn.infer_real_valued_columns_from_input(x_training_data)
 
-batch_size=100
-
-in_units=784
-out_units=10
-h1_units=300
-h2_units=200
-h3_units=100
+    feature_columns = tf.contrib.learn.infer_real_valued_columns_from_input(
+        x_training_data)
+    classifier = tf.contrib.learn.DNNClassifier(
+        feature_columns=feature_columns, hidden_units=[100, 50, 10], n_classes=10)
 
 
-
-W1=tf.Variable(tf.truncated_normal([in_units,h1_units],stddev=0.1))
-b1=tf.Variable(tf.zeros([h1_units]))
-W2=tf.Variable(tf.zeros([h1_units,h2_units]))
-b2=tf.Variable(tf.zeros([h2_units]))
-W3=tf.Variable(tf.zeros([h2_units,h3_units]))
-b3=tf.Variable(tf.zeros([h3_units]))
-W4=tf.Variable(tf.zeros([h3_units,out_units]))
-b4=tf.Variable(tf.zeros([out_units]))
+    classifier.fit(x_training_data, y_training_data, steps=200)
+    predictions = list(classifier.predict(x1, as_iterable=True))
+    score = metrics.accuracy_score(y1, predictions)
+    print('Accuracy: {0:f}'.format(score))
 
 
-x = tf.placeholder(tf.float32, [None, in_units])
-keep_prob=tf.placeholder(tf.float32)
-
-#hidden1=tf.nn.relu(tf.matmul(x,W1)+b1)
-#hidden1_drop=tf.nn.dropout(hidden1,keep_prob)
-
-hidden1=tf.nn.relu(tf.matmul(x,W1)+b1)
-hidden2=tf.nn.relu(tf.matmul(hidden1,W2)+b2)
-hidden3=tf.nn.relu(tf.matmul(hidden2,W3)+b3)
-
-hidden_drop=tf.nn.dropout(hidden3,keep_prob)
-
-y = tf.nn.softmax(tf.matmul(hidden_drop,W4) + b4)
-y_ = tf.placeholder(tf.float32, [None,out_units])
-
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_*tf.log(y),reduction_indices=[1]))
-train_step = tf.train.AdagradOptimizer(0.3).minimize(cross_entropy)
-
-init = tf.initialize_all_variables()
-sess = tf.Session()
-sess.run(init)
-
-
-
-for i in range(int(len(x_training_data)/batch_size)):
-    batch_xs=x_training_data[(i*batch_size):((i+1)*batch_size)]
-    batch_ys=y_training_data[(i*batch_size):((i+1)*batch_size)]
-
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys,keep_prob:0.75})
-
-
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-print(sess.run(accuracy, feed_dict={x: x1, y_: y1,keep_prob:1.0}))
+if __name__ == '__main__':
+  tf.app.run()
--- code/15-4.py
+++ code/15-4.py
@@ -1,32 +1,57 @@
 import tensorflow as tf
-import pickle
-import gzip
 from tensorflow.contrib.learn.python import learn
 from sklearn import metrics
+from sklearn.model_selection import train_test_split
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
 
-def load_data():
-    with gzip.open('../data/MNIST/mnist.pkl.gz') as fp:
-        training_data, valid_data, test_data = pickle.load(fp)
-    return training_data, valid_data, test_data
+#0,0.64,0.64,0,0.32,0,0,0,0,0,0,0.64,0,0,0,0.32,0,1.29,1.93,0,0.96,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+# 0,0,0,0,0.778,0,0,3.756,61,278,1
+def load_SpamBase(filename):
+    x=[]
+    y=[]
+    with open(filename) as f:
+        for line in f:
+            line=line.strip('\n')
+            v=line.split(',')
+            y.append(int(v[-1]))
+            t=[]
+            for i in range(57):
+                t.append(float(v[i]))
+            t=np.array(t)
+            x.append(t)
+
+    x=np.array(x)
+    y=np.array(y)
+    print x.shape
+    print y.shape
+
+    x_train, x_test, y_train, y_test=train_test_split( x,y, test_size=0.4, random_state=0)
+    print x_train.shape
+    print x_test.shape
+    return x_train, x_test, y_train, y_test
+
 
 
 def main(unused_argv):
-    training_data, valid_data, test_dat=load_data()
+    x_train, x_test, y_train, y_test=load_SpamBase("../data/spambase/spambase.data")
 
-    x_training_data,y_training_data=training_data
-    x1,y1=test_dat
 
-    feature_columns = learn.infer_real_valued_columns_from_input(x_training_data)
-
-    feature_columns = tf.contrib.learn.infer_real_valued_columns_from_input(
-        x_training_data)
+    feature_columns = tf.contrib.learn.infer_real_valued_columns_from_input(x_train)
     classifier = tf.contrib.learn.DNNClassifier(
-        feature_columns=feature_columns, hidden_units=[100, 50, 10], n_classes=10)
+        feature_columns=feature_columns, hidden_units=[30,10], n_classes=2)
 
 
-    classifier.fit(x_training_data, y_training_data, steps=200)
-    predictions = list(classifier.predict(x1, as_iterable=True))
-    score = metrics.accuracy_score(y1, predictions)
+    classifier.fit(x_train, y_train, steps=500,batch_size=10)
+    y_predict=list(classifier.predict(x_test, as_iterable=True))
+    #y_predict = classifier.predict(x_test)
+    #print y_predict
+    score = metrics.accuracy_score(y_test, y_predict)
+    print('Accuracy: {0:f}'.format(score))
+
+    gnb = GaussianNB()
+    y_predict = gnb.fit(x_train, y_train).predict(x_test)
+    score = metrics.accuracy_score(y_test, y_predict)
     print('Accuracy: {0:f}'.format(score))
 
 
